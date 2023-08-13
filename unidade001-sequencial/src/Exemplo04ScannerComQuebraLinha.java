71c71f6f204e07e3f8fb07bdebb3afffcd14ffc4
Eduardo <edumapri@gmail.com>
2023-08-13 18:20:14 -0300

    update

---
+++ unidade001-sequencial/src/Exemplo04ScannerComQuebraLinha.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Exemplo04ScannerComQuebraLinha {
+    public static void main(String[] args){
+
+        Scanner entradaUsuario = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite o numero de sua conta: ");
+        int conta = entradaUsuario.nextInt();
+
+        System.out.print("Digite o número de sua agência: ");
+        String agencia = entradaUsuario.next();
+
+        System.out.print("Digite o seu nome: ");
+        String nome = entradaUsuario.nextLine();
+
+        entradaUsuario.nextLine();
+
+        System.out.print("Informe o valor de seu depósito inicial: ");
+        double saldo = entradaUsuario.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia
+                + ", conta " + conta + " e o seu saldo " + saldo + " já está disponível para saque.");
+
+        entradaUsuario.close();
+
+    }
+}
